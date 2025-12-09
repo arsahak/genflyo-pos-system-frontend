92cbd01b5bd9ed7d789ec57dd3d2b06cb087fe92
arsahak <arsahak99@gmail.com>
2025-12-09 23:59:51 +0600

    fix build erorr

--- app/users/create-user/page.tsx
+++ app/users/create-user/page.tsx
@@ -1,5 +1,8 @@
 import NewUserCreate from "@/component/userManagement/NewUserCreate";
 
+// Force dynamic rendering to prevent SSR issues with browser APIs
+export const dynamic = 'force-dynamic';
+
 const page = () => {
   return (
     <div>
